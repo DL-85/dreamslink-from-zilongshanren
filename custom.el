a820407bde6f2bcd12ca6b3ad8221a7bccb662e5
DL-85 <g11061069@gmail.com>
2017-04-11 21:57:47 +0800

    It's a modified test.

--- custom.el
+++ custom.el
@@ -5,10 +5,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ahs-case-fold-search nil t)
+ '(ahs-case-fold-search nil)
  '(command-log-mode-window-size 50)
  '(company-dabbrev-minimum-length 3)
  '(company-dabbrev-other-buffers nil)
+ '(company-idle-delay 0.08)
  '(company-show-numbers t)
  '(company-statistics-auto-restore nil)
  '(ctags-update-delay-seconds 1024)
